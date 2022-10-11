86a3053114e4c6793f7ce27c74e0d7934097cdde
Saurav Shah <sauravshah.mail@gmail.com>
2022-10-12 00:45:33 +0545

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+header = st.container()
+form = st.container()
+result = st.container()
+plots = st.container()
+
+with header:
+    st.title('Predict Sine value using ML Algo')
