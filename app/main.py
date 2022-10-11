90a1047bb4dec23a801dc1b8689ada5ff88adce0
Saurav Shah <sauravshah.mail@gmail.com>
2022-10-12 02:08:27 +0545

    add form and chart

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
+import config
 import streamlit as st
+
+from css.cssHelper import local_css, remote_css, icon
+from sineTraining import fig
 
 header = st.container()
 form = st.container()
 result = st.container()
 plots = st.container()
 
+local_css("./app/css/style.css")
+remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+
 with header:
-    st.title('Predict Sine value using ML Algo')
+    st.title('Predict Sine values using ML Algo')
+
+with form:
+    st.text('Generate random sine values for training')
+    config.train_len = st.number_input('No. of train samples', 100)
+    st.write(fig)
+
+with result:
+    st.button('Train using this sample data')
